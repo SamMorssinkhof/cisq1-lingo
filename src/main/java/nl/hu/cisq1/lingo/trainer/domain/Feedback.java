166e6e37a0e8f70f08ac0059a9caeca13064fc73
Sam Morssinkhof <sam.morssinkhof@student.hu.nl>
2022-01-17 15:49:32 +0100

    added functionality to generate hint based on feedback and previous hint

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -1,6 +1,9 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Feedback {
     private String attempt;
@@ -13,5 +16,55 @@ public class Feedback {
 
     public boolean isWordGuessed(){
         return marks.stream().allMatch(i -> i == Mark.CORRECT);
+    }
+
+//    public List<Character> giveHint(List<Feedback> feedbackHistory, String wordToGuess){
+//        List<Character> hint = Arrays.asList(new Character[wordToGuess.length()]);
+//
+//        for(Feedback feedback : feedbackHistory){
+//            for(Mark mark : feedback.marks){
+//                if(mark == Mark.CORRECT){
+//                    int markIndex = feedback.marks.indexOf(mark);
+//                    hint.add(markIndex, feedback.attempt.charAt(markIndex));
+//                }
+//            }
+//        }
+//        return hint;
+//    }
+
+    public List<Character> giveHint(List<Character> previousHint, String wordToGuess){
+        if (marks.stream().allMatch(i -> i == Mark.INVALID)){
+            return previousHint;
+        } else {
+            for (int i = 0; i < wordToGuess.length()-1; i++){
+                if(previousHint.get(i) == '*' && marks.get(i) == Mark.CORRECT){
+                    previousHint.set(i, attempt.charAt(i));
+                }
+            }
+        }
+        return previousHint;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feedback feedback = (Feedback) o;
+        return Objects.equals(attempt, feedback.attempt) && Objects.equals(marks, feedback.marks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attempt, marks);
+    }
+
+    @Override
+    public String toString() {
+        return "Feedback{" +
+                "attempt='" + attempt + '\'' +
+                ", marks=" + marks +
+                '}';
     }
 }
--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -3,8 +3,14 @@ package nl.hu.cisq1.lingo.trainer.domain;
 import nl.hu.cisq1.lingo.words.domain.Word;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,5 +32,22 @@ class FeedbackTest {
         assertFalse(feedback.isWordGuessed());
     }
 
+
+    static Stream<Arguments> provideHintExamples() {
+        return Stream.of(
+                Arguments.of("straat", new Feedback("stroom", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.ABSENT, Mark.ABSENT, Mark.ABSENT)), Arrays.asList('s', 't', '*', '*', '*'), Arrays.asList('s', 't', 'r', '*', '*')),
+                Arguments.of("straat", new Feedback("strak", List.of(Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID)), Arrays.asList('s', 't', '*', '*', '*'), Arrays.asList('s', 't', '*', '*', '*')),
+                Arguments.of("straat", new Feedback("stoorm", List.of(Mark.CORRECT, Mark.CORRECT, Mark.ABSENT, Mark.ABSENT, Mark.PRESENT, Mark.ABSENT)), Arrays.asList('s', 't', '*', '*', '*'), Arrays.asList('s', 't', '*', '*', '*'))
+                );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideHintExamples")
+    @DisplayName("hint is correct if marks are correctly used when generating a new hint")
+    void correctHintIsGiven(String wordToGuess, Feedback feedback, List<Character> previousHint, List<Character> newHint){
+        List<Character> hint = feedback.giveHint(previousHint, wordToGuess);
+        assertEquals(newHint, hint);
+    }
+    
 
 }
